f953ae11022cdc95b3ee86dae247114a29dd2b6e
Neamul01 <neamulhaque2002@gmail.com>
2023-09-03 20:17:26 +0600

    product types added

---
+++ src/products/types/productTypes.ts
@@ -0,0 +1,22 @@
+export type Product = {
+  name: string;
+  description: string;
+  colors: Array<string>;
+  rating: number;
+  sizes: Array<string>;
+  quantity: number;
+  category: string;
+  sku: string;
+  features: string;
+  specification?: string;
+  additionalInfo?: string;
+  sizeGuide?: string;
+  reviews: Review[];
+};
+
+export type Review = {
+  name: string;
+  comment: string;
+  email: string;
+  rating: number | string;
+};
